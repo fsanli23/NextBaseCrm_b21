98b0c07456d6f3de607fb17919d6a480f9ee1fc3
awettedla <awettedla@gmail.com>
2021-02-06 22:14:36 -0500

    commit

--- src/test/java/com/nextBaseCrm/utilities/ConfigurationReader.java
+++ src/test/java/com/nextBaseCrm/utilities/ConfigurationReader.java
@@ -19,7 +19,7 @@ public class ConfigurationReader {
     }
 
     public static String getProperty(String keyWord) {
-        return properties.getProperty(keyWord);
+        return properties.getProperty(keyWord);//
 
 
     }
