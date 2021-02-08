cb3b58c0badb0d9147f7e8eaab454efc04537e57
glbhr <gulbahar.leblebicii@gmail.com>
2021-02-08 12:13:06 -0500

    webDriverFactory is ready

--- src/test/java/com/nextBaseCrm/utilities/webDriverFactory.java
+++ src/test/java/com/nextBaseCrm/utilities/webDriverFactory.java
@@ -1,4 +1,29 @@
 package com.nextBaseCrm.utilities;
 
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class webDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+        if (browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+
+            return new ChromeDriver();
+
+        }else if((browserType.equalsIgnoreCase("firefox"))) {
+            WebDriverManager.firefoxdriver().setup();
+
+            return new FirefoxDriver();
+        }else{
+            System.out.println("Given browser type does not exist. Driver = null!");
+            return null;
+        }
+    }
+
+
+    
 }
