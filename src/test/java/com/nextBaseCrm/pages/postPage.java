5d5fedd1ac12c0a3dc3c0183335c941492e45e47
AnaKeane <74145178+AnaKeane@users.noreply.github.com>
2021-02-10 11:14:46 -0600

    US7

---
+++ src/test/java/com/nextBaseCrm/pages/postPage.java
@@ -0,0 +1,11 @@
+package com.nextBaseCrm.pages;
+
+import com.nextBaseCrm.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class postPage {
+    public postPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+}
---
+++ src/test/java/com/nextBaseCrm/tests/US7_SeePostHomePage/LikeFollowEyeFavoriteCommentButtons.java
@@ -0,0 +1,11 @@
+package com.nextBaseCrm.tests.US7_SeePostHomePage;
+
+public class LikeFollowEyeFavoriteCommentButtons {
+
+}
+//1. users (hr, marketing and helpdesk) click "Like" button for any post
+//2. users are able to follow a post by clicking "Follow" button
+//3. users can see who are the people viewed a post with eye icon
+//4. users click star icon to save a post as favorite
+//5. users can write a comment to a post
+//6. users cancel a comment with "Cancel" button
