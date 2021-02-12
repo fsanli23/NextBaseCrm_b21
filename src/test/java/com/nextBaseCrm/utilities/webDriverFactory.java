afe8c2a08b7cca523cfe5e908ce58aa08e227d7a
fsanli23 <sanlifatih23@gmail.com>
2021-02-11 18:12:56 -0600

    first commit

--- src/test/java/com/nextBaseCrm/utilities/webDriverFactory.java
+++ src/test/java/com/nextBaseCrm/utilities/webDriverFactory.java
@@ -1,4 +1,18 @@
 package com.nextBaseCrm.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class webDriverFactory {
+
+
+
+    
+
+
+
+
+
 }
